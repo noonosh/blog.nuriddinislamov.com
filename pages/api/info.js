cdfc029eac3b5042bd3e6067c9422259d73e3e1f
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2021-09-29 11:00:19 +0100

    update api info

--- pages/api/info.js
+++ pages/api/info.js
@@ -6,11 +6,11 @@ export default function handler(req, res) {
 		phone: '998998789907',
 		spoken_languages: ['English', 'Russian', 'Uzbek'],
 		programming_in: ['Python', 'JavaScript', 'Swift'],
-		coding: ['HTML', 'CSS'],
 		moto: 'Everythin is EZ (e-a-s-y)',
 		hobbies: 'bookaholic. Loving philosophy reads',
 		telegram: 'https://t.me/nuriddin_i',
 		github: 'https://github.com/nuriddinislamov',
-		instagram: 'None',
+		twitter: 'https://twitter.com/nuriddinislamov',
+		instagram: null,
 	});
 }
